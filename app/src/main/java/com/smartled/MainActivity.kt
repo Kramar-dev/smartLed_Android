734f3a0fc412167c54b8d7123a06f9c50eaf8462
Iggi <i.i.kramarenko@gmail.com>
2022-05-15 20:31:41 +0200

    init repo

---
+++ app/src/main/java/com/smartled/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.smartled
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import io.ktor.client.*
+import io.ktor.client.plugins.websocket.*
+import io.ktor.http.*
+import io.ktor.websocket.*
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.Channel
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
